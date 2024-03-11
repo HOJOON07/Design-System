7c7247e99fffdb4a5989b595ae17919fddb352df
hojoon <ghwns107@naver.com>
2024-03-11 18:05:41 +0900

    api 설계 작업 완료

--- services/hotube/src/app/api/videos/popular-list/route.ts
+++ services/hotube/src/app/api/videos/popular-list/route.ts
@@ -1,5 +1,11 @@
-import { GetVideoPopularListRequestParams } from "@/features/main/api/getVideosPopularList";
+import {
+  GetVideoPopularListRequestParams,
+  GetVideoPopularListResponse,
+} from "@/features/main/api/getVideosPopularList";
 import { youtubeServerInstance } from "@/shared/api/youtube/server/instance";
+import { formatKoreanTextCompareDatesFromNow } from "@/shared/utils/format/data";
+import { formatNumberToKoreanText } from "@/shared/utils/format/number";
+import { youtube_v3 } from "googleapis";
 import { NextRequest } from "next/server";
 
 const parseQueryParams = (
@@ -8,6 +14,37 @@ const parseQueryParams = (
   return {
     maxResults: Number(params.get("maxResults") ?? "10"),
     pageToken: params.get("pageToken") ?? undefined,
+  };
+};
+
+const mappingResponse = (
+  data: youtube_v3.Schema$VideoListResponse,
+): GetVideoPopularListResponse => {
+  const lists =
+    data?.items?.map(({ id, snippet, statistics }) => {
+      const publishedAt = snippet?.publishedAt ?? "";
+      const parsedViewCount = parseInt(statistics?.viewCount ?? "0");
+      return {
+        videoId: id ?? "",
+        title: snippet?.title ?? "",
+        description: snippet?.description ?? "",
+        channelId: snippet?.channelId ?? "",
+        channelTitle: snippet?.channelTitle ?? "",
+        thumbnail: {
+          url: snippet?.thumbnails?.medium?.url ?? "",
+        },
+        publishedAt,
+        publishedAtDisplayText:
+          formatKoreanTextCompareDatesFromNow(publishedAt),
+        viewCount: parsedViewCount,
+        viewCountDisplayText: formatNumberToKoreanText(parsedViewCount, true),
+      };
+    }) ?? [];
+  return {
+    lists,
+    prevPageToken: data.prevPageToken ?? undefined,
+    nextPageToken: data.nextPageToken ?? undefined,
+    totalResults: data.pageInfo?.totalResults ?? 0,
   };
 };
 
@@ -21,10 +58,9 @@ export const GET = async (request: NextRequest) => {
       ...queryParams,
     });
 
-    console.log(request);
-    return Response.json({
-      data,
-    });
+    const mappedData = mappingResponse(data);
+
+    return Response.json(mappedData);
   } catch (err) {
     console.log(err);
     return new Response(
--- services/hotube/src/features/main/api/getVideosPopularList.ts
+++ services/hotube/src/features/main/api/getVideosPopularList.ts
@@ -1,3 +1,5 @@
+import { VideoThumbnail } from "@/shared/api/youtube/types/item";
+import { ListPageApiInfo } from "@/shared/api/youtube/types/list";
 import { youtube_v3 } from "googleapis";
 
 export type GetVideoPopularListRequestParams = Pick<
@@ -5,4 +7,19 @@ export type GetVideoPopularListRequestParams = Pick<
   "maxResults" | "pageToken"
 >;
 
-export type GetVideoPopularListResponse = {};
+export type PopularListItem = {
+  videoId: string;
+  title: string;
+  description: string;
+  channelId: string;
+  channelTitle: string;
+  thumbnail: VideoThumbnail;
+  publishedAt: string;
+  publishedAtDisplayText: string; // 1개월전
+  viewCount: number;
+  viewCountDisplayText: string; // 23만회
+};
+
+export type GetVideoPopularListResponse = {
+  lists: PopularListItem[];
+} & ListPageApiInfo;
